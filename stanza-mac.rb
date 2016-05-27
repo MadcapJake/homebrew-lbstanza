262e8ca7d636432119da39e4b1ed4b96a9473b7a
Jake Russo <madcap.russo@gmail.com>
2016-05-26 20:45:57 -0500

    Add stanza-mac formula

---
+++ stanza-mac.rb
@@ -0,0 +1,41 @@
+class LbstanzaMac < Formula
+  desc "An optionally-typed, general-purpose programming language from the University of California, Berkeley."
+  homepage "http://lbstanza.org"
+  url "http://lbstanza.org/resources/stanza/stanza.zip"
+  sha256 "a6bdb36af071ece657ebb59f198c08376cd4964a90592b5108b21dd3f3bb94bc"
+  head "https://github.com/StanzaOrg/lbstanza.git"
+  version "0.9.0"
+
+  def install
+    system("./stanza", "install", "-platform", "linux", "-path", prefix)
+
+    prefix.install "stanza",
+                   "compiler",
+                   "core",
+                   "fast-pkgs",
+                   "pkgs",
+                   "runtime"
+
+    bin.mkpath
+    ln "#{prefix}/stanza", "#{bin}/stanza"
+
+    inreplace "#{prefix}/.stanza" do |s|
+      s.gsub! /#{buildpath}/, prefix
+    end
+  end
+
+  test do
+    File.write("#{prefix}/test.stanza", "print(1)")
+    system "#{bin}/stanza", "compile", "#{prefix}/test.stanza", "-o", "#{prefix}/test"
+    system "#{prefix}/test"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following line to your .bashrc or .zshrc:
+      export STANZA_CONFIG="#{prefix}"
+    If you are using Fish Shell, run this command:
+      set -Ux STANZA_CONFIG "#{prefix}"
+    EOS
+  end
+
+end

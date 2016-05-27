f427991cdbecce9f50b0e0ac747e8dd940b3e639
Jake Russo <madcap.russo@gmail.com>
2016-05-26 20:30:44 -0500

    Add lbstanza formula

---
+++ lbstanza.rb
@@ -0,0 +1,41 @@
+class Lbstanza < Formula
+  desc "An optionally-typed, general-purpose programming language from the University of California, Berkeley."
+  homepage "http://lbstanza.org"
+  url "http://lbstanza.org/resources/stanza/lstanza.zip"
+  sha256 "1cafb878d7b749dfb35dabfcc68fe7e390896a83de3236112eb6900c5c9ccd7f"
+  head "https://github.com/StanzaOrg/lbstanza.git"
+  version "0.9.0"
+
+  def install
+    system("./stanza", "install", "-platform", "linux", "-path", prefix)
+
+    prefix.install "stanza",
+                   "compiler",
+                   "core",
+                   "fast-pkgs",
+                   "pkgs",
+                   "runtime"
+
+    bin.mkpath
+    ln "#{prefix}/stanza", "#{bin}/stanza"
+
+    inreplace "#{prefix}/.stanza" do |s|
+      s.gsub! /#{buildpath}/, prefix
+    end
+  end
+
+  test do
+    File.write("#{prefix}/test.stanza", "print(1)")
+    system "#{bin}/stanza", "compile", "#{prefix}/test.stanza", "-o", "#{prefix}/test"
+    system "#{prefix}/test"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following line to your .bashrc or .zshrc:
+      export STANZA_CONFIG="#{prefix}"
+    If you are using Fish Shell, run this command:
+      set -Ux STANZA_CONFIG "#{prefix}"
+    EOS
+  end
+
+end

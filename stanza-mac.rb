4048775376b96220e5411c50b11829c9443afd5b
Michael Bartnett <michaelbartnett@users.noreply.github.com>
2016-06-03 15:58:50 -0400

    change LbstanzaMac to StanzaMac
    Homebrew complained that the class name didn't match the package name (this also mirrors the StanzaLinux formula)

--- stanza-mac.rb
+++ stanza-mac.rb
@@ -1,4 +1,4 @@
-class LbstanzaMac < Formula
+class StanzaMac < Formula
   desc "An optionally-typed, general-purpose programming language from the University of California, Berkeley."
   homepage "http://lbstanza.org"
   url "http://lbstanza.org/resources/stanza/stanza.zip"
